41d6da86f79a7f199171f2e71842fefc8a6dd21b
minhe51805 <truongminh0949@gmail.com>
2026-04-03 22:58:27 +0700

    ds

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,13 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://minhtruong.dev",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
+      lastModified: "2026-04-03",
+      changeFrequency: "weekly",
       priority: 1,
     },
   ];
